cc679acea4f308af3141a34200e2a178417975aa
Eloy <shelomora60@gmail.com>
2025-07-17 13:13:42 +0800

    update tambokikay

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+// App.js
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="sapot-mode">
+      <h1>Fuck you tambokikay</h1>
+      <h2>Uto-uto</h2>
+    </div>
+  );
+}
+
+export default App;
