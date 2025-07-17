7608539d50748f8610c3aa2088b9b7d86a998324
Eloy <shelomora60@gmail.com>
2025-07-17 14:54:14 +0800

    27months

---
+++ 27months/src/App.js
@@ -0,0 +1,109 @@
+import React, { useState, useEffect } from 'react';
+import './css.css';
+import image1 from "./images/1.jpeg";
+import image2 from "./images/1.jpeg";
+import image3 from "./images/1.jpeg";
+import image4 from "./images/1.jpeg";
+import image5 from "./images/1.jpeg";
+import image6 from "./images/1.jpeg";
+import image7 from "./images/1.jpeg";
+import image8 from "./images/1.jpeg";
+import image9 from "./images/1.jpeg";
+import image10 from "./images/1.jpeg";
+import image11 from "./images/1.jpeg";
+import image12 from "./images/1.jpeg";
+import image13 from "./images/1.jpeg";
+import image14 from "./images/1.jpeg";
+import image15 from "./images/1.jpeg";
+import image16 from "./images/1.jpeg";
+import image17 from "./images/1.jpeg";
+import image18 from "./images/1.jpeg";
+import image19 from "./images/1.jpeg";
+import image20 from "./images/1.jpeg";
+import image21 from "./images/1.jpeg";
+import image22 from "./images/1.jpeg";
+import image23 from "./images/1.jpeg";
+import image24 from "./images/1.jpeg";
+import image25 from "./images/1.jpeg";
+import image26 from "./images/1.jpeg";
+import image27 from "./images/1.jpeg";
+
+function App() {
+  const [showModal, setShowModal] = useState(false);
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      const modal = document.getElementById('messageModal');
+      if (event.target === modal) {
+        setShowModal(false);
+      }
+    };
+    window.addEventListener('click', handleClickOutside);
+    return () => window.removeEventListener('click', handleClickOutside);
+  }, []);
+
+  return (
+    <div>
+      <div id="hedlinecontainer">
+        <div id="shine">Happy 27th Monthsary Tambokikay💖</div>
+      </div>
+
+      <div className="gallery">
+        <img src={image1} />
+        <img src={image2} />
+        <img src={image3} />
+        <img src={image4} />
+        <img src={image5} />
+        <img src={image6} />
+        <img src={image7} />
+        <img src={image8} />
+        <img src={image9} />
+        <img src={image10} />
+        <img src={image11} />
+        <img src={image12} />
+        <img src={image13} />
+        <img src={image14} />
+        <img src={image15} />
+        <img src={image16} />
+        <img src={image17} />
+        <img src={image18} />
+        <img src={image19} />
+        <img src={image20} />
+        <img src={image21} />
+        <img src={image22} />
+        <img src={image23} />
+        <img src={image24} />
+        <img src={image25} />
+        <img src={image26} />
+        <img src={image27} />
+      </div>
+
+      <button className="msg-btn" onClick={() => setShowModal(true)}>📩 View Message</button>
+
+      {showModal && (
+        <div id="messageModal" className="modal" style={{ display: 'flex' }}>
+          <div className="modal-content">
+            <span className="close" onClick={() => setShowModal(false)}>&times;</span>
+            <h2>Happy Monthsary 💌</h2>
+            <p>
+              Thank you for all the memories, love, and moments we've shared. 💕<br />
+              I’m so grateful to have you in my life. Here's to many more months and years together! 💖
+            </p>
+          </div>
+        </div>
+      )}
+
+      {/* Falling hearts */}
+      <div className="heart" style={{ left: '10%', animationDelay: '0s' }}></div>
+      <div className="heart" style={{ left: '20%', animationDelay: '2s' }}></div>
+      <div className="heart" style={{ left: '30%', animationDelay: '4s' }}></div>
+      <div className="heart" style={{ left: '40%', animationDelay: '1s' }}></div>
+      <div className="heart" style={{ left: '50%', animationDelay: '3s' }}></div>
+      <div className="heart" style={{ left: '60%', animationDelay: '5s' }}></div>
+      <div className="heart" style={{ left: '70%', animationDelay: '6s' }}></div>
+      <div className="heart" style={{ left: '80%', animationDelay: '2s' }}></div>
+    </div>
+  );
+}
+
+export default App;
